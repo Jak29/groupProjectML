e58b2e3e8e64f07fc179f82ea60ecc86ec41fadb
Jak Cullinane <jak.cullinane@mycit.ie>
2022-11-16 18:22:35 +0000

    can feed back predictions to continue making new predictions into the future

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ import pandas as pd
 import numpy as np
 import pandas_datareader.data as web
 import datetime as dt
+from datetime import timedelta
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 import matplotlib.pyplot as plt
-# import yfinance as yf
 
-# Load Data
-company = "MSFT"
+# Load Training Data
+company = "GOOG"
 start = dt.datetime(2015,1,1)
-end = dt.datetime(2021,1,1)
+end = dt.datetime(2022,1,1)
 data = web.DataReader(company, "yahoo", start, end)
 
 
@@ -53,14 +53,14 @@ model.add(Dense(units=1))
 
 
 model.compile(optimizer="adam", loss="mean_squared_error")
-model.fit(x_train, y_train, epochs=25, batch_size=32)
+model.fit(x_train, y_train, epochs=15, batch_size=32)
 
 # ========================================
 # TEST THE MODEL ACCURACY ON EXISTING DATA
 # ========================================
 
-# Load Test Data
-test_start = dt.datetime(2020,1,1)
+# Load Data
+test_start = dt.datetime(2022,1,1)
 test_end = dt.datetime.now()
 
 test_data = web.DataReader(company, "yahoo", test_start, test_end)
@@ -68,40 +68,59 @@ actual_prices = test_data["Close"].values
 
 total_dataset = pd.concat((data["Close"], test_data["Close"]), axis = 0)
 
-model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+# loop from here?
+counter = 280
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values # Important
 model_inputs = model_inputs.reshape(-1, 1)
 model_inputs = scaler.transform(model_inputs)
 
+for x in range(prediction_days):
+    
+    
+    # Make Predictions on Test Data
+    x_test = []
+    
+    for x in range(prediction_days, len(model_inputs)):
+        x_test.append(model_inputs[x-prediction_days:x, 0])
+    
+    x_test = np.array(x_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    
+    predicted_prices = model.predict(x_test)
+    predicted_prices = scaler.inverse_transform(predicted_prices)
+    
+    
+    # Plot the Test Predictions
+    plt.plot(actual_prices, color="black", label=f"Actual {company} Price")
+    plt.plot(predicted_prices, color="green", label=f"Predicted {company} Price")
+    plt.title(f"{company} Share Price")
+    plt.xlabel("Days")
+    plt.ylabel(f"{company} Share Price")
+    plt.legend()
+    plt.show()
+    
+    
+    # Predict Next Day
+    
+    real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs+1), 0]]
+    real_data = np.array(real_data)
+    real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+    
+    prediction = model.predict(real_data)
+    prediction = scaler.inverse_transform(prediction)
+    print(f"Prediction: {prediction}")
+    
 
-# Make Predictions on Test Data
-
-x_test = []
-
-for x in range(prediction_days, len(model_inputs)):
-    x_test.append(model_inputs[x-prediction_days:x, 0])
-
-x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-predicted_prices = model.predict(x_test)
-predicted_prices = scaler.inverse_transform(predicted_prices)
+    model_inputs = np.concatenate((model_inputs, prediction))
 
 
-# Plot the Test Predictions
 plt.plot(actual_prices, color="black", label=f"Actual {company} Price")
 plt.plot(predicted_prices, color="green", label=f"Predicted {company} Price")
 plt.title(f"{company} Share Price")
-plt.xlabel("Time")
+plt.xlabel("Days")
 plt.ylabel(f"{company} Share Price")
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
 
 
 
